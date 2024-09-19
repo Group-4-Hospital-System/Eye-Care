9087dd0c943bb3422c53bdeb2096fca3cef1d4ab
zeyadfaiyoumi <zeyadfaiyoumi@gmail.com>
2024-09-19 10:57:12 +0300

    Fix  Cookie Bug

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const pool = require('./config/db'); // Import the database pool
-const authRoutes = require('./routes/authRoutes'); // Import your auth routes
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const pool = require("./config/db"); // Import the database pool
+const authRoutes = require("./routes/authRoutes"); // Import your auth routes
+const cookieParser = require("cookie-parser");
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 // Test the database connection
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error('Error acquiring client', err.stack);
+    return console.error("Error acquiring client", err.stack);
   }
-  console.log('Connected to the PostgreSQL database');
+  console.log("Connected to the PostgreSQL database");
   release();
 });
 
 // Use routes (your route imports will go here)
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 
