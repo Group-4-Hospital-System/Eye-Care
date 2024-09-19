4815d3463d1cb134ba19f5ef3cf3fcf5a8dce60d
ramighazzawi2002 <rami.ghazzawiabed@gmail.com>
2024-09-19 16:53:55 +0300

    finish doctor dashboard design

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,14 +3,19 @@ import Home from "./pages/Home/Home"; // صفحة البداية
 import Login from "./features/auth/Login"; // مكون تسجيل الدخول
 import Register from "./features/auth/Register"; // مكون التسجيل
 import CheckoutPage from "./pages/Checkout";
-{/* Admin Dashboard */}
-import Dashboard from './pages/Admin-Dashboard/Page/Home/Home';
-import UserDashboard from './pages/Admin-Dashboard/Page/Users/UserDashboard';
-import DoctorsDashboard from './pages/Admin-Dashboard/Page/Doctors/DoctorsDashboard';
-import ContactUsDashboard from './pages/Admin-Dashboard/Page/ContactUs/ContactUsDashboard';
-import AppointmentsDashboard from './pages/Admin-Dashboard/Page/Appointments/Appointments';
- {/* End Of Admin Dashboard */}
-
+{
+  /* Admin Dashboard */
+}
+import Dashboard from "./pages/Admin-Dashboard/Page/Home/Home";
+import UserDashboard from "./pages/Admin-Dashboard/Page/Users/UserDashboard";
+import DoctorsDashboard from "./pages/Admin-Dashboard/Page/Doctors/DoctorsDashboard";
+import ContactUsDashboard from "./pages/Admin-Dashboard/Page/ContactUs/ContactUsDashboard";
+import AppointmentsDashboard from "./pages/Admin-Dashboard/Page/Appointments/Appointments";
+import DoctorDashboard from "./pages/doctorDashboard/doctor";
+{
+  /* End Of Admin Dashboard */
+}
+import "./index.css";
 
 function App() {
   return (
@@ -24,8 +29,15 @@ function App() {
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Dashboard/users" element={<UserDashboard />} />
         <Route path="/Dashboard/Doctors" element={<DoctorsDashboard />} />
-        <Route path="/Dashboard/Appointments" element={<AppointmentsDashboard />} />
+        <Route
+          path="/Dashboard/Appointments"
+          element={<AppointmentsDashboard />}
+        />
         <Route path="/Dashboard/ContactUS" element={<ContactUsDashboard />} />
+        <Route
+          path="/Dashboard/DoctorDashboard"
+          element={<DoctorDashboard />}
+        />
         {/* End Of Admin Dashboard */}
       </Routes>
     </Router>
