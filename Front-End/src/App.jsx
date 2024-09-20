799ad795a30fcc1aa2b78f62366db07a8c20a6ae
AbdelRahman-Alwabarneh <abdelRahmanalwabarneh@gmail.com>
2024-09-20 06:17:14 +0300

    adminDashboard

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,14 +3,18 @@ import Home from "./pages/Home/Home"; // صفحة البداية
 import Login from "./features/auth/Login"; // مكون تسجيل الدخول
 import Register from "./features/auth/Register"; // مكون التسجيل
 import CheckoutPage from "./pages/Checkout";
-{/* Admin Dashboard */}
-import Dashboard from './pages/Admin-Dashboard/Page/Home/Home';
-import UserDashboard from './pages/Admin-Dashboard/Page/Users/UserDashboard';
-import DoctorsDashboard from './pages/Admin-Dashboard/Page/Doctors/DoctorsDashboard';
-import ContactUsDashboard from './pages/Admin-Dashboard/Page/ContactUs/ContactUsDashboard';
-import AppointmentsDashboard from './pages/Admin-Dashboard/Page/Appointments/Appointments';
- {/* End Of Admin Dashboard */}
+{
+  /* Admin Dashboard */
+}
+import Dashboard from "./pages/Admin-Dashboard/Page/Home/Home";
+import UserDashboard from "./pages/Admin-Dashboard/Page/Users/UserDashboard";
+import DoctorsDashboard from "./pages/Admin-Dashboard/Page/Doctors/DoctorsDashboard";
+import ContactUsDashboard from "./pages/Admin-Dashboard/Page/ContactUs/ContactUsDashboard";
+import AppointmentsDashboard from "./pages/Admin-Dashboard/Page/Appointments/Appointments";
 
+{
+  /* End Of Admin Dashboard */
+}
 
 function App() {
   return (
@@ -24,8 +28,12 @@ function App() {
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Dashboard/users" element={<UserDashboard />} />
         <Route path="/Dashboard/Doctors" element={<DoctorsDashboard />} />
-        <Route path="/Dashboard/Appointments" element={<AppointmentsDashboard />} />
+        <Route
+          path="/Dashboard/Appointments"
+          element={<AppointmentsDashboard />}
+        />
         <Route path="/Dashboard/ContactUS" element={<ContactUsDashboard />} />
+
         {/* End Of Admin Dashboard */}
       </Routes>
     </Router>
